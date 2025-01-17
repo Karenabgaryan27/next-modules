22c5f3023c1212849a092550e0aa8db3062af82a
karen <karenabgaryan27@gmail.com>
2025-01-17 16:27:44 +0400

    unused states removed

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { DropdownMenuDemo } from "@/components/index.js";
